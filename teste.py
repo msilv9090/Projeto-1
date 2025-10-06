e436e0276003a34320ee143ba98486539a31a726
msilv9090 <ms8554020@gmail.com>
2025-10-06 15:28:33 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.write("Alô mundo")
 st.tittle("Meu programa")
 
-nome = st.text_input("Digite o seu nome")
+nome = st.text_input("Digite o seu nome: ")
 if nome:
    st.write(nome.upper())
