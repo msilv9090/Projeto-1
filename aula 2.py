0416479aec338bc9d3aba7abd475686aa48636f3
msilv9090 <ms8554020@gmail.com>
2025-10-13 15:12:59 -0300

    Update aula 2.py

--- aula 2.py
+++ aula 2.py
@@ -3,4 +3,12 @@ import stramlit as st
 
 dataset = pd.read_csv('https://www.irdx.com.br/media/uploads/paises.csv')
 
-st.dataframe(dataset)
+import plotly.express as px
+fig = px.scatter_geo(dataset,
+                     lat=dataset['latitude'],
+                     lon=dataset['longitude'],
+                     hover_name=dataset['nome'])
+
+fig.update_layout(title='Mapa Coroplético dos Países', geo_scope='world')
+
+st.plotly_chart(fig, use_container_width=True, theme="streamlit)
